3d60f772de475c839d7eff508dddbb0c380ba430
Louis Gasnault <louis.gasnault@epita.fr>
2021-11-29 14:27:07 +0000

    RobotGUI: routine with frequency

--- robot_gui/src/robot_gui/gui.py
+++ robot_gui/src/robot_gui/gui.py
@@ -1,12 +1,28 @@
 from Tkinter import *
 import ttk
 
+from ctypes import *
+from math import floor
+from time import sleep
+
 import rospy
 from robot_main.msg import Routine
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, UInt8
+
+from lmshid_bindings import lib
 
 set_routine = None
 start_routine = None
+
+def to_ghz(freq_10hz):
+    return freq_10hz / 1.0e8 
+
+# ttk.Spinbox is missing in Python 3.6
+class Spinbox(ttk.Entry):
+    def __init__(self, master=None, **kw):
+        ttk.Entry.__init__(self, master, "ttk::spinbox", **kw)
+    def set(self, value):
+        self.tk.call(self._w, "set", value)
 
 class App(ttk.Frame):
     def __init__(self, master=None):
@@ -14,9 +30,20 @@ class App(ttk.Frame):
 
         self.nb_steps = IntVar()
         self.nb_steps.set(1)
+        self.nb_steps.trace("w", self.nb_steps_changed)
 
         self.step_distance = DoubleVar()
         self.step_distance.set(1.0)
+
+        self.progress_var = IntVar()
+        self.progress_var.set(0)
+
+        self.freq_var = DoubleVar()
+        self.freq_var.set(to_ghz(lib.fnLMS_GetFrequency(1)))
+
+        self.freq_min = to_ghz(lib.fnLMS_GetMinFreq(1))
+        self.freq_max = to_ghz(lib.fnLMS_GetMaxFreq(1))
+        print(self.freq_min, self.freq_max)
 
         self.create_widgets()
 
@@ -29,7 +56,9 @@ class App(ttk.Frame):
         self.nb_steps_entry = ttk.Entry(self, textvariable=self.nb_steps)
         self.step_distance_entry = ttk.Entry(self, textvariable=self.step_distance)
 
-        self.progress = ttk.Progressbar(self, mode='determinate')
+        freq_select_button = ttk.Button(self, text='Freq', command=self.ask_freq)
+
+        self.progress = ttk.Progressbar(self, mode='determinate', variable=self.progress_var)
 
         start_button = ttk.Button(self, text='Start', command=self.start_routine)
         stop_button = ttk.Button(self, text='Stop', command=self.stop_routine)
@@ -38,9 +67,10 @@ class App(ttk.Frame):
         step_dist_label.grid(column=1, row=0, sticky=W)
         self.nb_steps_entry.grid(column=0, row=1)
         self.step_distance_entry.grid(column=1, row=1)
+        freq_select_button.grid(column=2, row=0, columnspan=2)
         start_button.grid(column=2, row=1)
         stop_button.grid(column=3, row=1)
-        self.progress.grid(column=0, row=2, columnspan=4, sticky=W+E)
+        self.progress.grid(column=0, row=3, columnspan=4, sticky=W+E)
 
         for child in self.winfo_children():
             child.grid(padx=5, pady=5)
@@ -48,16 +78,42 @@ class App(ttk.Frame):
     def start_routine(self):
         nb_steps = int(self.nb_steps_entry.get())
         step_distance = self.step_distance.get()
+        freq = int(floor(self.freq_var.get() * 1e8))
 
-        print("Routine: {0} steps of {1}m".format(nb_steps, step_distance))
-        set_routine.publish(Routine(nb_steps, step_distance))
+        set_routine.publish(Routine(nb_steps, step_distance, freq))
         start_routine.publish(True)
 
     def stop_routine(self):
         start_routine.publish(False)
 
+    def step_routine(self, msg):
+        self.progress_var.set(msg.data)
+
+    def ask_freq(self):
+        def dismiss():
+            dlg.grab_release()
+            dlg.destroy()
+
+        dlg = Toplevel(self)
+
+        ttk.Label(dlg, text='Frequency (GHz)').grid(sticky=W)
+        Spinbox(dlg, from_=self.freq_min, to=self.freq_max, increment=0.1, textvariable=self.freq_var, format="%.3f").grid() 
+        ttk.Button(dlg, text='Done', command=dismiss).grid(padx=12, pady=12)
+
+        dlg.protocol("WM_DELETE_WINDOW", dismiss)
+        dlg.wait_visibility()
+        dlg.grab_set()
+
+    def nb_steps_changed(self, *args):
+        try:
+            self.progress['maximum'] = self.nb_steps.get()
+            self.progress_var.set(0)
+        except ValueError:
+            pass
+
 def main():
     global set_routine, start_routine
+    devices_array = c_uint * 10
 
     # ROS
     rospy.init_node("robot_gui")
@@ -65,11 +121,34 @@ def main():
     set_routine = rospy.Publisher('set_routine', Routine, queue_size=1)
     start_routine = rospy.Publisher('start_routine', Bool, queue_size=1)
 
+    # LMS
+    lib.fnLMS_Init()
+    lib.fnLMS_SetTestMode(False)
+
+    num_devices = lib.fnLMS_GetNumDevices()
+    
+    active_devices = devices_array()
+    num_actives = lib.fnLMS_GetDevInfo(active_devices)
+
+    if num_actives == 0:
+        print("No active device")
+        return
+
+    source_id = active_devices[0]
+
+    status = lib.fnLMS_InitDevice(source_id)
+    print("Source: {0}".format(lib.fnLMS_perror(status).decode('UTF-8')))
+
+    print("Please wait.")
+    sleep(1)
+
     # Interface
     root = Tk()
     root.title("Robot GUI")
 
-    App(root)
+    app = App(root)
+
+    rospy.Subscriber('routine_progress', UInt8, app.step_routine)
 
     root.mainloop()
 
--- robot_main/src/main.cpp
+++ robot_main/src/main.cpp
@@ -1,26 +1,38 @@
+#include <unistd.h>
 #include <string>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <dynamic_reconfigure/server.h>
 #include <robot_main/GlobalConfig.h>
 #include "robot_driver/robot_driver.h"
-#include "robot_main/routine.h"
+#include "robot_main/Routine.h"
 #include "robot_main/data_exporter.h"
 
+robot_main::Routine routine;
 bool execute_routine = false;
 
-void reconfigure(robot_main::GlobalConfig &config, uint32_t level, RobotDriver &rd, Routine &routine)
+void reconfigure(robot_main::GlobalConfig &config, uint32_t level, RobotDriver &rd)
 {
     rd.reconfigure(config.scan_range, config.correction_threshold, config.turn_speed, config.drive_speed, config.target_distance);
-    routine.reconfigure(config.steps, config.step_distance);
 }
 
-void routine_callback(const std_msgs::Bool::ConstPtr &msg)
+void set_routine_callback(const robot_main::RoutineConstPtr &msg)
 {
-    execute_routine = msg->data;
+    ROS_INFO("Received routine: %i steps of %fm", msg->nb_steps, msg->step_distance);
+    
+    routine.nb_steps = msg->nb_steps;
+    routine.step_distance = msg->step_distance;
+    routine.freq = msg->freq;
+}
 
-    if (msg->data)
-        ROS_INFO("RobotMain: routine started");
+void start_routine_callback(const std_msgs::Bool &msg)
+{
+    execute_routine = msg.data;
+}
+
+void button_callback(const std_msgs::UInt8 &msg)
+{
+    ROS_INFO("Routine: %i steps of %fm ; Execute: %s", routine.nb_steps, routine.step_distance, execute_routine ? "yes" : "no");
 }
 
 int main(int argc, char **argv)
@@ -35,17 +47,24 @@ int main(int argc, char **argv)
     RobotDriver rd(nh);
 
     // Routine init
-    Routine routine;
+    routine.nb_steps = 0;
+    routine.step_distance = 0.0;
+    routine.freq = 0;
 
     // Subscribers
-    ros::Subscriber sub_buttons = nh.subscribe("/buttons", 1, &RobotDriver::button_input, &rd);
-    ros::Subscriber sub_routine = nh.subscribe("/routine", 1, &routine_callback);
+    // ros::Subscriber sub_buttons = nh.subscribe("/buttons", 1, &RobotDriver::button_input, &rd);
+    ros::Subscriber sub_buttons = nh.subscribe("/buttons", 1, &button_callback);
+    ros::Subscriber sub_set_routine = nh.subscribe("/set_routine", 1, &set_routine_callback);
+    ros::Subscriber sub_start_routine = nh.subscribe("/start_routine", 1, &start_routine_callback);
+
+    // Publisher
+    ros::Publisher pub_routine_progress = nh.advertise<std_msgs::UInt8>("/routine_progress", 1);
 
     // Dynamic reconfigure
     dynamic_reconfigure::Server<robot_main::GlobalConfig> server;
     dynamic_reconfigure::Server<robot_main::GlobalConfig>::CallbackType f;
 
-    f = boost::bind(&reconfigure, _1, _2, boost::ref(rd), boost::ref(routine));
+    f = boost::bind(&reconfigure, _1, _2, boost::ref(rd));
     server.setCallback(f);
 
     std::string report_folder = "/home/husarion/ros_workspace/reports/";
@@ -59,32 +78,47 @@ int main(int argc, char **argv)
         ros::spinOnce();
         loop_rate.sleep();
 
-        if (execute_routine && current_step < routine.steps)
+        if (execute_routine && current_step < routine.nb_steps)
         {
-            ROS_DEBUG("RobotMain: routine step %i, angle correction...", current_step + 1);
+            // ROS_DEBUG("RobotMain: routine step %i, angle correction...", current_step + 1);
 
-            CorrectionReport report = rd.correct_angle();
+            // CorrectionReport report = rd.correct_angle();
 
             // Export report
-            std::string file_name = report_folder + std::to_string(current_step) + extension;
-            export_correction_report(report, file_name);
+            // std::string file_name = report_folder + std::to_string(current_step) + extension;
+            // export_correction_report(report, file_name);
 
             // Export laser scan after correction
-            file_name = report_folder + std::to_string(current_step) + after_correction_suffix + extension;
-            auto scan = ros::topic::waitForMessage<sensor_msgs::LaserScan>("/scan");
-            export_laser_scan(scan, file_name);
+            // file_name = report_folder + std::to_string(current_step) + after_correction_suffix + extension;
+            // auto scan = ros::topic::waitForMessage<sensor_msgs::LaserScan>("/scan");
+            // export_laser_scan(scan, file_name);
+
+            ROS_INFO("RobotMain: routine step %i, emit frequency %i", current_step + 1, routine.freq);
+
+            // TODO
 
             ROS_DEBUG("RobotMain: routine step %i, driving to next stop", current_step + 1);
 
-            rd.drive(routine.step_distance);
+            // rd.drive(routine.step_distance);
 
-            ROS_DEBUG("RobotMain: routine step %i done", current_step + 1);
+            ROS_INFO("RobotMain: routine step %i done", current_step + 1);
 
             current_step ++;
+            
+            auto msg = std_msgs::UInt8();
+            msg.data = current_step;
+            pub_routine_progress.publish(msg);
+
+            sleep(1);
         }
-        else if (current_step == routine.steps)
+        else if (current_step == routine.nb_steps)
         {
             execute_routine = false;
+            current_step = 0;
+        }
+        else if (!execute_routine && current_step != 0)
+        {
+            // In case of stop from GUI, TODO: be able choose between "pause" or "stop" the routine
             current_step = 0;
         }
     }
